0b559ad8564a2a68d948dabdb0a9cffd528bd347
Rexford Nkansah <seanmavley@gmail.com>
2018-04-16 11:05:30 +0000

    updates

--- src/app/transaction/transaction-routing.module.ts
+++ src/app/transaction/transaction-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { DetailComponent } from './detail/detail.component';
+import { ListComponent } from './list/list.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'd/:transaction_id', component: DetailComponent },
+  { path: 't', component: ListComponent },
 ];
 
 @NgModule({
--- src/app/transaction/transaction.module.ts
+++ src/app/transaction/transaction.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { TransactionRoutingModule } from './transaction-routing.module';
 import { HomeComponent } from './home/home.component';
+import { DetailComponent } from './detail/detail.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   imports: [
     CommonModule,
     TransactionRoutingModule
   ],
-  declarations: [HomeComponent]
+  declarations: [HomeComponent, DetailComponent, ListComponent]
 })
 export class TransactionModule { }
